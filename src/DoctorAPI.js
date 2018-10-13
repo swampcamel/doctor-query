e9b116ab1db1b31cdf74fec9834beaaaaae015d4
Dan Mace <dmacebeta@gmail.com>
2018-10-12 17:03:15 -0700

    hide yo keys, hide yo wife

--- src/DoctorAPI.js
+++ src/DoctorAPI.js
@@ -2,7 +2,7 @@ export default class DoctorAPI {
   getDoctorInfo(search) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${search}&location=or-portland&skip=2&user_key=4ebf5c067a1c173eeaf2246e8fc85ae8`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${search}&location=or-portland&skip=2&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
